5f5942582e8eaa64370bdddd272310c2279d55d8
Tanner York <tannerwyork@gmail.com>
2019-11-06 11:09:45 -0800

    Update model to use new preprocessed sentiment data

--- model.py
+++ model.py
@@ -6,13 +6,13 @@ import tensorflow_hub as hub
 import tensorflow_datasets as tfds
 import pandas as pd
 
-training_dataframe = pd.read_csv('./data/training_data.csv',  encoding="ISO-8859-1", names=['target', 'text'])
+training_dataframe = pd.read_csv('./data/training_data.csv',  encoding="ISO-8859-1", names=['Sentiment', 'SentimentText'])
 training_dataframe = training_dataframe.dropna()
 
-validation_dataframe = pd.read_csv('./data/training_data.csv', encoding="ISO-8859-1", names=['target', 'text'])
+validation_dataframe = pd.read_csv('./data/training_data.csv', encoding="ISO-8859-1", names=['Sentiment', 'SentimentText'])
 validation_dataframe = validation_dataframe.dropna()
 
-test_dataframe = pd.read_csv('./data/training_data.csv', encoding="ISO-8859-1", names=['target', 'text'])
+test_dataframe = pd.read_csv('./data/training_data.csv', encoding="ISO-8859-1", names=['Sentiment', 'SentimentText'])
 test_dataframe = test_dataframe.dropna()
 
 embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
@@ -20,15 +20,16 @@ hub_layer = hub.KerasLayer(embedding, input_shape=[], dtype=tf.string, trainable
 
 model = tf.keras.Sequential()
 model.add(hub_layer)
-model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(40, activation='relu'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 model.summary()
 
-model.compile(optimizer=SGD(ls=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-history = model.fit(training_dataframe['text'].values, training_dataframe['target'].values, 
-                    validation_data=(validation_dataframe['text'].values, validation_dataframe['target'].values), 
-                    batch_size=512, epochs=20, verbose=1)
-results = model.evaluate(test_dataframe['text'].values, test_dataframe['target'].values, verbose=2)
+history = model.fit(training_dataframe['SentimentText'].values, training_dataframe['Sentiment'].values, 
+                    validation_data=(validation_dataframe['SentimentText'].values, validation_dataframe['Sentiment'].values), 
+                    batch_size=512, epochs=100, verbose=1)
+results = model.evaluate(test_dataframe['SentimentText'].values, test_dataframe['Sentiment'].values, verbose=2)
 for name, value in zip(model.metrics_names, results):
   print("%s: %.3f" % (name, value))
+model.save('tweet-sentiment.h5')
